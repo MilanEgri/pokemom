b785560c810d9c2b63d2d5c55c10471f1b1b2f56
Milán Egri <117757061+MilanEgri@users.noreply.github.com>
2023-04-11 10:38:40 +0200

    oclicnk runing on its own

--- src/commponents/Start.js
+++ src/commponents/Start.js
@@ -3,17 +3,37 @@ import React, { useEffect, useState } from 'react'
 const Start = () => {
 
   const [locations, setLocations] = useState(null);
-
   useEffect(() => {
     fetch('https://pokeapi.co/api/v2/location')
     .then(res => res.json())
     .then(data=> setLocations(data))
   }, [])
 
+  async function getRadomPokeURLFromLocation(item) {
+    const response = await fetch(`https://pokeapi.co/api/v2/location-area/${item}`);
+    const json = await response.json();
+    const randomPokemonURL = json.pokemon_encounters[Math.floor(Math.random() * json.pokemon_encounters.length)].pokemon.url;
+    getPokeDataFromURL(randomPokemonURL)
+  }
+  async function getPokeDataFromURL(url) {
+    const response = await fetch(url);
+    const json = await response.json();
+    const randomPokemon = json;
+    //setActivePokemon(randomPokemon)
+    console.log(randomPokemon);
+  }
+
+  function onClickHandler(i) {
+ 
+    getRadomPokeURLFromLocation(i + 1)
+    //setActiveView('poke')
+
+  }
+
   return (
     <div>
       {locations && locations.results.map((location, index) => (
-        <h2 key={index}>{location.name}</h2>
+        <button key={index} onClick={onClickHandler(index)}>{location.name}</button>
       ))}
     </div>
   )
