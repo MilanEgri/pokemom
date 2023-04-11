5db751ae54f09b69a369cd780433688f178c3aae
Milán Egri <117757061+MilanEgri@users.noreply.github.com>
2023-04-11 12:02:21 +0200

    removed testing useffect

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { useState,useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 import Start from './components/Start';
 import Pokemon from './components/Pokemon';
