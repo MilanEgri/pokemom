e04c4dbda7a0d75f14d07e329612dca710edcabf
Milán Egri <117757061+MilanEgri@users.noreply.github.com>
2023-04-11 11:30:54 +0200

    users pokemons rendered

--- src/commponents/Pokemon.js
+++ src/commponents/Pokemon.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Pokemon = ({pokemon}) => {
-  return (
+    const usersPokemon = [
+        "https://pokeapi.co/api/v2/pokemon/bulbasaur",
+        "https://pokeapi.co/api/v2/pokemon/charizard",
+        "https://pokeapi.co/api/v2/pokemon/poliwhirl"
+    ]
+    const [usersPokemons, setUsersPokemons]=useState([])
+    useEffect(() => {
+        const promises = usersPokemon.map(url => fetch(url));
+
+        Promise.all(promises)
+            .then(responses => Promise.all(responses.map(response => response.json())))
+            .then(pokemonData => setUsersPokemons(pokemonData))
+      }, [])
+  
+    return (
     <div>
       <h1>{pokemon.name}</h1>
+      <h3>{usersPokemons && usersPokemons.map((e,i) => <div key={i}>{e.name}</div>)}</h3>
     </div>
   )
 }
