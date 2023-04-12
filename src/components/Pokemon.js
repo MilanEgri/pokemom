6cab28a80e1c972572508a0265fe8e2841acd02b
Milán Egri <117757061+MilanEgri@users.noreply.github.com>
2023-04-12 10:48:50 +0200

    starting css

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,19 +1,38 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
+import '../App.css'
 
-const Pokemon = ({pokemon,setUserPokemon,setPage,usersPokemons}) => {
+const Pokemon = ({ pokemon, setUserPokemon, setPage, usersPokemons }) => {
 
 
-  function handleOnclick (i) {
+  function handleOnclick(i) {
     setUserPokemon(usersPokemons[i])
     setPage('battle')
   }
-  
-    return (
-    <div>
-      <h1>{pokemon.name}</h1>
-      <div >{usersPokemons && usersPokemons.map((e,i) => <button onClick={() => handleOnclick(i)} key={i}>{e.name}</button>)}</div>
+
+  return (
+    <div className='pokemonCardContanier'>
+      <div className='pokemonCard'>
+        <div className='pokemonBackground'>
+          <img src={pokemon.sprites.front_default} className='image' />
+        </div>
+        <div className='pokemonContent'>
+          <h1 className='pokemonName'>{pokemon.name}</h1>
+          <h2 className='pokemonType'>{pokemon["types"][0]["type"]["name"]}</h2>
+        </div>
+        <div className='pokemonStats'>
+          <h3 className='pokemonHP'>HP: {pokemon.stats[0].base_stat}</h3>
+          <h3 className='pokemonATK'>ATK: {pokemon.stats[1].base_stat}</h3>
+          <h3 className='pokemonDEF'>DEF: {pokemon.stats[2].base_stat}</h3>
+        </div>
+      </div>
+
+
+
+
+      <div >{usersPokemons && usersPokemons.map((e, i) => <button onClick={() => handleOnclick(i)} key={i}>{e.name}</button>)}</div>
     </div>
   )
 }
 
 export default Pokemon
+
