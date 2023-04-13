1d2349d8bd2524ff81c1990d322cd6863d2ef5fb
Milan Egri <117757061+MilanEgri@users.noreply.github.com>
2023-04-13 11:31:47 +0200

    pokemon page working

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -11,6 +11,8 @@ const Pokemon = ({ pokemon, setUserPokemon, setPage, usersPokemons }) => {
   }
 
   return (
+    <div className='pokemon'>
+      <h1 className='enemy'>Enemy Pokemon</h1>
     <div className={`pokemonCardContanier${pokemon["types"][0]["type"]["name"]}`}>
       <div className='pokemonCard'>
         <div className={`pokemonBackground`}>
@@ -24,8 +26,9 @@ const Pokemon = ({ pokemon, setUserPokemon, setPage, usersPokemons }) => {
           <p className='pokemonDEF'>{pokemon.stats[2].base_stat}</p>
         </div>
       </div>
+    </div>
 
-
+      <h1 className='userPokemon'>Chose a Pokemon to Battle</h1>
 
 
       <div className='user' >{usersPokemons && usersPokemons.map((e, i) => <button onClick={() => handleOnclick(i)} key={i} className={`smallPokemonCardContanier${e["types"][0]["type"]["name"]}`} >
@@ -43,7 +46,7 @@ const Pokemon = ({ pokemon, setUserPokemon, setPage, usersPokemons }) => {
         </div>
       </button>)}
       </div>
-    </div>
+    </div >
   )
 }
 
